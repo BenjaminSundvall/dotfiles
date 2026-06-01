f48327d6753a5677b2d5ce00eda647033690bdb7
Benjamin Sundvall <bensu30@liu.se>
2026-06-01 17:36:37 +0200

    [vim] Changed theme to gruvbox

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -397,19 +397,27 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
-  ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
-    styles = {
-      comments = { italic = false }, -- Disable italics in comments
+
+  vim.pack.add { gh 'ellisonleao/gruvbox.nvim' }
+  require('gruvbox').setup {
+    italic = {
+      strings = false,
+      emphasis = false,
+      comments = false,
+      operators = false,
+      folds = false,
+    },
+    overrides = {
+      MiniStatuslineModeNormal = { fg = '#282828' },
+      MiniStatuslineModeVisual = { fg = '#282828' },
+      MiniStatuslineModeCommand = { fg = '#282828' },
     },
   }
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
-  -- vim.cmd.colorscheme 'retrobox'
+  vim.cmd.colorscheme 'gruvbox'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
