7d0c0b8fc1215153ccbca47b5307462c104bbc56
Benjamin Sundvall <bensu30@liu.se>
2026-07-02 16:11:17 +0200

    [hypr] Added support for workspace naming

---
+++ hypr/.config/hypr/config/workspaces.lua
@@ -0,0 +1,28 @@
+-- Workspaces https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+
+-------------------------
+---- Workspace Names ----
+-------------------------
+
+-- Browser 󰖟
+-- Terminal  
+-- Notes 󰤌
+-- Email 󰇮
+-- Code 
+-- Messaging 󰭹
+-- Game 󰮂 󰊴
+-- AI 
+-- Steam 
+-- Discord
+-- Teams 󰊻
+
+hl.workspace_rule({ workspace = "1",  default_name = "" })
+hl.workspace_rule({ workspace = "2",  default_name = "" })
+hl.workspace_rule({ workspace = "3",  default_name = "" })
+hl.workspace_rule({ workspace = "4",  default_name = "" })
+hl.workspace_rule({ workspace = "5",  default_name = "" })
+hl.workspace_rule({ workspace = "6",  default_name = "" })
+hl.workspace_rule({ workspace = "7",  default_name = "" })
+hl.workspace_rule({ workspace = "8",  default_name = "" })
+hl.workspace_rule({ workspace = "9",  default_name = "" })
+hl.workspace_rule({ workspace = "10", default_name = "" })
